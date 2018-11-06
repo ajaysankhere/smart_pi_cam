4a4e699b83d7b4eb5543e3845b2d117be1083b82
ajaysankhere <44524892+ajaysankhere@users.noreply.github.com>
2018-11-06 23:08:09 +0530

    updated email
    Updated email credentials

--- mail.py
+++ mail.py
@@ -2,18 +2,12 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
-# from email.MIMEMultipart import MIMEMultipart
-# from email.MIMEText import MIMEText
-# from email.MIMEImage import MIMEImage
 
-# Email you want to send the update from (only works with gmail)
-fromEmail = 'devesh.tiwari12141@gmail.com'
-# You can generate an app password here to avoid storing your password in plain text
-# https://support.google.com/accounts/answer/185833?hl=en
-fromEmailPassword = 'Sitaram12'
+fromEmail = 'cameratestai@gmail.com'
+fromEmailPassword = 'Sovereign848'
 
 # Email you want to send the update to
-toEmail = 'devesh.tiwari605@gmail.com'
+toEmail = 'ajay12sankhere@gmail.com'
 
 def sendEmail(image):
 	msgRoot = MIMEMultipart('related')
